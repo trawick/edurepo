454adb2c20d232f41107fef981e31a7f35139929
Jeff Trawick <trawick@gmail.com>
2014-02-17 13:47:33 -0500

    CORS-enable the teachers API
    references:
    
    https://gist.github.com/robhudson/3848832
    http://codeispoetry.me/index.php/make-your-django-tastypie-api-cross-domain/

--- src/edurepo/teachers/api.py
+++ src/edurepo/teachers/api.py
@@ -2,8 +2,46 @@ from tastypie import fields
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from teachers.models import Teacher, TeacherClass, Entry
 
+#### from https://gist.github.com/robhudson/3848832
 
-class TeacherResource(ModelResource):
+from django.http import HttpResponse
+from tastypie.exceptions import ImmediateHttpResponse
+
+
+class CORSResource(object):
+    """
+    Adds CORS headers to resources that subclass this.
+    """
+    def create_response(self, *args, **kwargs):
+        response = super(CORSResource, self).create_response(*args, **kwargs)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Headers'] = 'Content-Type'
+        return response
+
+    def method_check(self, request, allowed=None):
+        if allowed is None:
+            allowed = []
+
+        request_method = request.method.lower()
+        allows = ','.join(map(str.upper, allowed))
+
+        if request_method == 'options':
+            response = HttpResponse(allows)
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Headers'] = 'Content-Type'
+            response['Allow'] = allows
+            raise ImmediateHttpResponse(response=response)
+
+        if not request_method in allowed:
+            response = http.HttpMethodNotAllowed(allows)
+            response['Allow'] = allows
+            raise ImmediateHttpResponse(response=response)
+
+        return request_method
+#### end gist
+
+
+class TeacherResource(CORSResource, ModelResource):
 
     class Meta:
         queryset = Teacher.objects.all()
@@ -12,9 +50,11 @@ class TeacherResource(ModelResource):
             'email': ALL,
             'name': ALL,
         }
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get']
 
 
-class TeacherClassResource(ModelResource):
+class TeacherClassResource(CORSResource, ModelResource):
 
     teacher = fields.ForeignKey(TeacherResource, 'teacher')
 
@@ -26,9 +66,11 @@ class TeacherClassResource(ModelResource):
             'name': ALL_WITH_RELATIONS,
             'course_id': ALL,
         }
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get']
 
 
-class EntryResource(ModelResource):
+class EntryResource(CORSResource, ModelResource):
 
     teacher = fields.ForeignKey(TeacherResource, 'teacher', full=True)
     teacher_class = fields.ForeignKey(TeacherClassResource, 'teacher_class', full=True)
@@ -42,3 +84,5 @@ class EntryResource(ModelResource):
             'date': ALL,
             'teacher_name': ALL_WITH_RELATIONS
         }
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get']
