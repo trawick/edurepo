1953a95bf497bb9d695a1348eee503d42e153fcb
Jeff Trawick <trawick@gmail.com>
2014-02-22 08:46:59 -0500

    add basic API for resources

--- src/edurepo/resources/urls.py
+++ src/edurepo/resources/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import include, patterns, url
 from resources import views
+from resources.api import ResourceResource
+
+resource_resource = ResourceResource()
 
 urlpatterns = patterns('',
+                       (r'^api/', include(resource_resource.urls)),
                        url(r'^$', views.index, name='resources/index'),
                        url(r'^(?P<resource_id>[^/]+)/$', views.detail, name='detail'),
                        )
