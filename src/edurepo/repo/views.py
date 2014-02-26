025338f5cfc65035721d494d618f6be36934fe47
Jeff Trawick <trawick@gmail.com>
2014-02-26 16:44:11 -0500

    improve views for repo app

--- src/edurepo/repo/views.py
+++ src/edurepo/repo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.template import RequestContext
-from repo.models import Course, LearningObjective, GlossaryItem, TrueFalseItem
+from repo.models import Course, ICan, LearningObjective, MultipleChoiceItem, GlossaryItem, ReferenceText, TrueFalseItem
 
 
 def index(request):
@@ -17,8 +17,20 @@ def detail(request, course_id):
 
 
 def by_objective(request, course_id, objective_id):
+    objective = LearningObjective.objects.get(id=objective_id)
+    ican_items = ICan.objects.filter(learning_objective=objective_id)
     glossary_items = GlossaryItem.objects.filter(learning_objective=objective_id)
+    multiple_choice_items = MultipleChoiceItem.objects.filter(learning_objective=objective_id)
     tf_items = TrueFalseItem.objects.filter(learning_objective=objective_id)
-    context = RequestContext(request, {'glossary_items': glossary_items,
+    reference_text = ReferenceText.objects.filter(learning_objective=objective_id)
+    if reference_text:
+        reference_text = reference_text[0]
+    context = RequestContext(request, {'course_id': course_id,
+                                       'objective': objective,
+                                       'objective_id': objective_id,
+                                       'reference_text': reference_text,
+                                       'glossary_items': glossary_items,
+                                       'ican_items': ican_items,
+                                       'multiple_choice_items': multiple_choice_items,
                                        'tf_items': tf_items})
     return render(request, 'repo/objective.html', context)
