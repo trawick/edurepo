c17cc45d56256eae48154cbbe7a7d6045378fa68
Jeff Trawick <trawick@gmail.com>
2014-03-06 07:35:21 -0700

    move CORSResource class to a new core module

--- src/edurepo/teachers/api.py
+++ src/edurepo/teachers/api.py
@@ -1,45 +1,7 @@
 from tastypie import fields
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from teachers.models import Teacher, TeacherClass, Entry
-
-#### from https://gist.github.com/robhudson/3848832
-
-from django.http import HttpResponse
-from tastypie import http
-from tastypie.exceptions import ImmediateHttpResponse
-
-
-class CORSResource(object):
-    """
-    Adds CORS headers to resources that subclass this.
-    """
-    def create_response(self, *args, **kwargs):
-        response = super(CORSResource, self).create_response(*args, **kwargs)
-        response['Access-Control-Allow-Origin'] = '*'
-        response['Access-Control-Allow-Headers'] = 'Content-Type'
-        return response
-
-    def method_check(self, request, allowed=None):
-        if allowed is None:
-            allowed = []
-
-        request_method = request.method.lower()
-        allows = ','.join(map(str.upper, allowed))
-
-        if request_method == 'options':
-            response = HttpResponse(allows)
-            response['Access-Control-Allow-Origin'] = '*'
-            response['Access-Control-Allow-Headers'] = 'Content-Type'
-            response['Allow'] = allows
-            raise ImmediateHttpResponse(response=response)
-
-        if not request_method in allowed:
-            response = http.HttpMethodNotAllowed(allows)
-            response['Allow'] = allows
-            raise ImmediateHttpResponse(response=response)
-
-        return request_method
-#### end gist
+from core.utils import CORSResource
 
 
 class TeacherResource(CORSResource, ModelResource):
