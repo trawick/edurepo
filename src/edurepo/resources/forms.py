a2c5f0f94ed24be0d723dce42d052fbf389000e2
Jeff Trawick <trawick@gmail.com>
2014-03-05 19:38:29 -0700

    better field labels on resource creation form

--- src/edurepo/resources/forms.py
+++ src/edurepo/resources/forms.py
@@ -4,6 +4,9 @@ from models import Resource
 
 class ResourceForm(forms.ModelForm):
 
+    objective = forms.CharField(label="Learning objective")
+    url = forms.CharField(label="URL of resource")
+
     class Meta:
         model = Resource
         fields = ('objective', 'url', 'notes')
