e2e9e1d2a9b1b183be73b5ccc254239ba3ffa7cc
Jeff Trawick <trawick@gmail.com>
2014-03-11 08:06:00 -0400

    allow filtering by learning objective id (AGAIN)

--- src/edurepo/resources/api.py
+++ src/edurepo/resources/api.py
@@ -2,15 +2,18 @@ from tastypie import fields
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from resources.models import Resource
 from core.utils import CORSResource
+from repo.api import LearningObjectiveResource
 
 
 class ResourceResource(CORSResource, ModelResource):
+
+    objective = fields.ForeignKey(LearningObjectiveResource, 'objective', full=False)
 
     class Meta:
         queryset = Resource.objects.all()
         resource_name = 'resource'
         filtering = {
-            'objective': ALL,
+            'objective': ALL_WITH_RELATIONS,
         }
         list_allowed_methods = ['get']
         detail_allowed_methods = ['get']
