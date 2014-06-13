3e21853b36cdb8be50e782ff8150908223615047
Jeff Trawick <trawick@gmail.com>
2014-06-13 17:32:38 -0400

    Test repo lookup functions

--- src/edurepo/core/tests.py
+++ src/edurepo/core/tests.py
@@ -1,5 +1,7 @@
+import os
+import unittest
 from django.test import TestCase
-from utils import ellipsis
+from utils import description_for_objective, ellipsis, objectives_for_course
 
 
 class BasicTests(TestCase):
@@ -10,3 +12,15 @@ class BasicTests(TestCase):
         self.assertEquals(ellipsis(long_str, len(long_str) - 1), long_str[:-4] + '...')
         self.assertEquals(ellipsis(long_str, len(long_str) + 1), long_str)
         self.assertEquals(ellipsis(long_str, 100), long_str)
+
+    @unittest.skipIf(not 'TEST_PROVIDER' in os.environ,
+                     "Test case can't work without TEST_PROVIDER pointing to API provider")
+    def test_objective_lookup(self):
+        desc = description_for_objective('MG4-FACTMULT', os.environ['TEST_PROVIDER'])
+        self.assertTrue('factors and multiples' in desc)
+
+    @unittest.skipIf(not 'TEST_PROVIDER' in os.environ,
+                     "Test case can't work without TEST_PROVIDER pointing to API provider")
+    def test_course_lookup(self):
+        res = objectives_for_course('MG4', os.environ['TEST_PROVIDER'])
+        self.assertTrue('MG4-FACTMULT' in [y for (y, z) in res])
