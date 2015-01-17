27782346dcb5c8d29953c52d36db20db1020f7aa
Jeff Trawick <trawick@gmail.com>
2015-01-17 09:22:26 -0500

    shortcut

---
+++ src/ansible/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+usage="Usage: $0 {prod|staging}"
+if test $# -ne 1; then
+    echo $usage 1>&2
+    exit 1
+fi
+
+if test $1 != "prod"; then
+    if test $1 != "staging"; then
+        echo $usage 1>&2
+        exit 1
+    fi
+fi
+
+. ~/envs/ansible/bin/activate
+exec ansible-playbook -i $HOME/server-config/$1/edurepo/ansible/hosts deploy.yml
