15ee6c5142cf1426062f6453ff11c45e7e0ef742
Jeff Trawick <trawick@gmail.com>
2015-04-21 07:14:26 -0400

    standard deploy.sh, supports pass-through of Ansible options

--- src/ansible/deploy.sh
+++ src/ansible/deploy.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-usage="Usage: $0 {prod|staging}"
-if test $# -ne 1; then
+PROJECT=edurepo
+
+usage="Usage: $0 {prod|staging} [ansible-options]"
+if test $# -lt 1; then
     echo $usage 1>&2
     exit 1
 fi
 
-if test $1 != "prod"; then
-    if test $1 != "staging"; then
+SERVER=$1
+shift
+
+if test $SERVER != "prod"; then
+    if test $SERVER != "staging"; then
         echo $usage 1>&2
         exit 1
     fi
 fi
 
 . ~/envs/ansible/bin/activate
-exec ansible-playbook -i $HOME/server-config/$1/edurepo/ansible-settings deploy.yml
+exec ansible-playbook $* -i $HOME/server-config/${SERVER}/${PROJECT}/ansible-settings deploy.yml
