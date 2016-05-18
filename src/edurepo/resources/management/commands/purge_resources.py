eac807687fed95542684c8f0df65a6f0893070d8
Jeff Trawick <trawick@gmail.com>
2016-05-17 20:45:05 -0400

    convert purge_links to management command

--- src/edurepo/resources/management/commands/purge_resources.py
+++ src/edurepo/resources/management/commands/purge_resources.py
@@ -1,18 +1,10 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'edurepo.settings')
-
-import sys
-sys.path.append('.')
-
-import django
-from django.utils.timezone import utc
 from datetime import datetime, timedelta
-from optparse import OptionParser
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+from django.utils.timezone import now
+
 from resources.models import Resource, ResourceVerification
-
-
-def now():
-    return datetime.utcnow().replace(tzinfo=utc)
 
 
 def purge_unreachable_resources(debug=False, min_success_time=None, purge_stranded=False):
@@ -53,24 +45,28 @@ def purge_inappropriate_resources(debug):
         print 'Consider purging inappropriate resource %s (%d)' % (resource, resource.inappropriate_flags)
 
 
-django.setup()
-parser = OptionParser()
-parser.add_option("-d", "--debug", dest="debug",
-                  action="store_true",
-                  help="show debug messages")
-parser.add_option("--purge-stranded", action="store_true",
-                  help="automatically purge stranded resource verification records")
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option(
+            "--debug", dest="debug",
+            action="store_true",
+            help="show debug messages",
+        ),
+        make_option(
+            "--purge-stranded", action="store_true",
+            help="automatically purge stranded resource verification records",
+        ),
+    )
 
-(options, args) = parser.parse_args()
+    def handle(self, *args, **options):
+        # Identify resources for removal based on failed verification.
+        #
+        # The time delta needs to be in sync with validate_links, or
+        # we'll identify resources for purge that haven't been tried
+        # recently.
+        purge_unreachable_resources(debug=options['debug'],
+                                    min_success_time=now() - timedelta(days=14),
+                                    purge_stranded=options['purge_stranded'])
 
-# Identify resources for removal based on failed verification.
-#
-# The time delta needs to be in sync with validate_links, or
-# we'll identify resources for purge that haven't been tried
-# recently.
-purge_unreachable_resources(debug=options.debug,
-                            min_success_time=now() - timedelta(days=14),
-                            purge_stranded=options.purge_stranded)
-
-# Identify resources for removal based on inappropriate flags.
-purge_inappropriate_resources(options.debug)
+        # Identify resources for removal based on inappropriate flags.
+        purge_inappropriate_resources(options['debug'])
--- src/edurepo/resources/server_purge.sh
+++ src/edurepo/resources/server_purge.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd $HOME/git/edurepo
 . envs/edurepo/bin/activate
+export DJANGO_SETTINGS_MODULE=edurepo.settings
 cd src/edurepo
-python resources/purge_links.py --purge-stranded $*
+python ./manage.py purge_resources --purge-stranded $*
